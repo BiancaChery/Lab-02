4fcb70133237d9f422a0c675dbad8047e38bbf17
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-04-18 00:54:26 +0000

    Here is the full completed assignment with corrections.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,53 @@
 alert("Hello Good People")
 prompt("What is your name?")
 
-// TODO: Ask the user's name.
-
-// TODO: Ask 5 y/n questions.
-let answerOne = prompt("Am I happy?");
-let answerTwo = prompt("Am I married?")
-let answerThree = prompt("Do I own a shop?")
-let answerFour = prompt("Am I an only child?")
-let answerFive = prompt("Do I have children?")
+let answerOne = prompt("Am I happy?") .toLowerCase()
+let answerTwo = prompt("Am I married?") .toLowerCase()
+let answerThree = prompt("Do I own a shop?") .toLowerCase()
+let answerFour = prompt("Am I an only child?") .toLowerCase()
+let answerFive = prompt("Do I have children?") .toLowerCase()
 
 if(answerOne === 'y' || answerOne ==='yes'){
-    console.log("You are correct!");
+    // console.log("You are correct!"); 
+    alert("You are correct!");
 } else { 
     // if 'n' then you are incorrect
-    console.log("Nope, wrong-o!");
+   // console.log("Nope, wrong-o!");
+   alert("Nope wrong-o!");
 }
 
 if(answerTwo === 'n' || answerTwo === 'no'){
-    console.log("You are correct!");
+   // console.log("You got it!");
+   alert("You got it!")
 } else {
     // if 'y' then you are incorrect
-    console.log("That ain't the move!");
+   // console.log("That ain't the move!");
+   alert("That ain't the move!");
 }
 
 if(answerThree === 'n' || answerThree === 'no'){
-    console.log("You are correct!");
+   // console.log("Oh snap, that's it!");
+   alert("Oh snap, that's it!");
 } else {
     //if 'y' then you are incorrect
-    console.log("Hmm, no sir!");
+   // console.log("Hmm, no sir!");
+   alert("Hmm, no sir!");
 }
 
 if(answerFour === 'n' || answerFour === 'no'){
-    console.log("You are correct!");
+   // console.log("You're learning me I see!");
+   alert("You're learning me I see!")
 } else {
     // if 'y' then you are incorrect
-    console.log("Almost had it!");
+   // console.log("Almost had it!");
+   alert("Almost had it!");
 }
 
 if(answerFive === 'y' || answerFive === 'yes'){
-    console.log("You are correct!");
+   // console.log("Aye that's it!");
+   alert("Aye that's it!");
 } else {
     //if 'n' then you are incorrect
-    console.log("Eh, wrong answer!")
+   // console.log("Eh, wrong answer!");
+   alert("Eh, wrong answer!")
 }
